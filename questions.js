d913f4bcc201c7509a3f846e8a38de73f943da51
notingtheacceleration <54728866+notingtheacceleration@users.noreply.github.com>
2019-08-30 19:36:50 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,536 @@
+questions = [
+	//revolution vs reform
+    {
+        "question": "Reforming capitalist society to achieve better rights for workers is acceptable and desirable.",
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+	},
+	{
+		"question": "Revolution is the only way of achieving a socialist society.",
+        "effect": {
+            "a": 10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        },
+	},
+	{
+		"question": "The negative consequences of a revolution outweigh the advantages.",
+        "effect": {
+            "a": -10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+	},
+	{
+		"question": "Liberal democracy is a viable way of achieving a socialist society.",
+        "effect": {
+            "a": -10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+	},
+	{
+		"question": "A socialist revolution is made inevitable by the conditions of capitalism.",
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+	},
+	{
+		"question": "Workplace democracy within capitalism is an acceptable alternative to a complete socialization of the economy.",
+        "effect": {
+            "a": -10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+	},
+	{
+		"question": "Revolutionary violence is acceptable as long as the final outcome is positive.",
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+	},
+	{
+		"question": "Redistributing wealth away from the wealthy through taxes is a viable way of defeating inequality.",
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	//scientific vs utopian
+	{
+		"question": "Material conditions and needs are the dominant drive behind socioeconomic changes.",
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Socialism can only be fully achieved in developed industrialized societies.",
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "It is possible to peacefully convince the ruling class to conform to a socialist society.",
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Capitalism will induce its own demise through contradictions that result in crisis.",
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Class conflict is a force that has influenced much of history.",
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "The establishment of socialism is a gradual process, rather than an instant or rapid one.",
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Society is chiefly driven by individuals and ideas.",
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Total economic and social equality is possible to achieve.",
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	//central vs decentral
+	{
+		"question": "An economy is designed better when it is organized bottom-up rather than top-down.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -10,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Local planners, rather than national planners, are more efficient at running a planned economy.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "A centrally planned economy based around computers is a concept worth investigating.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "In underdeveloped societies a centrally planned economy is the only way to make rapid progress.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 10,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "The principles of workplace democracy and self-management are desirable.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "A highly centralized planned economy is not socialism but rather state capitalism.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Bureaucracy and inefficiency are inherent in centrally planned economies.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -10,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "The government should be capable of assigning individuals to workplaces.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 10,
+            "d": 0,
+			"e": 0,
+			"f": 0
+        }
+    },
+	//international vs national
+	{
+		"question": "A world socialist republic is a realistic and desirable goal.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Nationalism and patriotism are impulses that are unacceptable in a socialist society.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 10,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "The global socialist movement should be led by a single party or organization.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Foreign officials have no right to dictate policy in another country.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -5,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Open borders between likeminded socialist countries is desirable.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "Mass immigration policies are naïve and should not be implemented.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -10,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "People of similar cultures should unite in federations.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+			"e": 0,
+			"f": 0
+        }
+    },
+	{
+		"question": "The international proletariat belongs to no country.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 10,
+			"e": 0,
+			"f": 0
+        }
+    },
+	//party vs union
+	{
+		"question": "Socialist political parties should participate in liberal democratic elections.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 5,
+			"f": 0
+        }
+    },
+	{
+		"question": "Trade unions and workers councils should form the basis of a socialist society.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": -5,
+			"f": 0
+        }
+    },
+	{
+		"question": "The leadership of political parties in the progression towards socialism will always lead to dictatorship.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": -10,
+			"f": 0
+        }
+    },
+	{
+		"question": "Democratic centralism is an authoritarian organizational structure that disregards the masses.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": -5,
+			"f": 0
+        }
+    },
+	{
+		"question": "Only a mass workers party can achieve any meaningful goals.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 10,
+			"f": 0
+        }
+    },
+	{
+		"question": "Mass spontaneous actions are more effective than carefully planned actions.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": -5,
+			"f": 0
+        }
+    },
+	{
+		"question": "The fact that so many workers vote for bourgeois parties over socialist parties is proof that party politics are no longer relevant.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": -10,
+			"f": 0
+        }
+    },
+	{
+		"question": "Socialist organizations are better off when organized loosely and decentrally.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": -10,
+			"f": 0
+        }
+    },
+	//industry vs ecology
+	{
+		"question": "Climate change is a major global threat that all socialists must fiercly combat.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": -5
+        }
+    },
+	{
+		"question": "Measures to defeat climate change are unacceptable if they result in significant decreases in production and quality of life.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 10
+        }
+    },
+	{
+		"question": "Renewable sources of energy are incapable of efficiently replacing fossil fuels.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": 5
+        }
+    },
+	{
+		"question": "Industrialized farming practices must be abolished.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": -5
+        }
+    },
+	{
+		"question": "It will be important to implement extensive environmental protections as part of achieving socialism.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": -10
+        }
+    },
+	{
+		"question": "Experimental environmentally-friendly food sources like cultured meat are worth investing in.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": -5
+        }
+    },
+	{
+		"question": "Personal vehicles should be replaced with free public transport.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": -5
+        }
+    },
+	{
+		"question": "We must accept that the socialist states of the 20th century failed to seriously address environmental issues.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0,
+			"f": -5
+        }
+    }
+];
